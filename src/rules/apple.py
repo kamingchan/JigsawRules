bf6fd0ab57a4ea5b6d6d31b243b29e8f79c226e3
Terry Chan <terry@32ph.com>
2017-09-23 02:30:07 +0800

    update rules for Apple

--- src/rules/apple.py
+++ src/rules/apple.py
@@ -4,6 +4,8 @@ from rules.rules import Rules
 class AppleMaps(Rules):
     def __str__(self):
         return '''# Rule for Apple Maps
+USER-AGENT,*locationd*,{proxy_group}
+USER-AGENT,*geod*,{proxy_group}
 DOMAIN-SUFFIX,ls.apple.com,{proxy_group} // Apple Maps location detective, use local maps
 '''.format(
             proxy_group=self.proxy_group
@@ -13,6 +15,7 @@ DOMAIN-SUFFIX,ls.apple.com,{proxy_group} // Apple Maps location detective, use l
 class iCloud(Rules):
     def __str__(self):
         return '''# Rule for iCloud
+USER-AGENT,*CloudKit*,{proxy_group}
 DOMAIN-SUFFIX,icloud.com,{proxy_group}
 '''.format(
             proxy_group=self.proxy_group
@@ -22,6 +25,7 @@ DOMAIN-SUFFIX,icloud.com,{proxy_group}
 class AppleMusic(Rules):
     def __str__(self):
         return '''# Rule for Apple Music
+USER-AGENT,*Music*,{proxy_group}
 DOMAIN-SUFFIX,se2.itunes.apple.com,{proxy_group}
 DOMAIN-SUFFIX,radio.itunes.apple.com,{proxy_group}
 DOMAIN-SUFFIX,radio-activity.itunes.apple.com,{proxy_group}
@@ -31,6 +35,15 @@ DOMAIN-SUFFIX,streamingaudio.itunes.apple.com,{proxy_group}
 DOMAIN-SUFFIX,search.itunes.apple.com,{proxy_group}
 DOMAIN-SUFFIX,api.itunes.apple.com,{proxy_group}
 DOMAIN-SUFFIX,audio.itunes.apple.com,{proxy_group}
+'''.format(
+            proxy_group=self.proxy_group
+        )
+
+
+class Podcast(Rules):
+    def __str__(self):
+        return '''# Rule for Podcast App
+USER-AGENT,*Podcasts*,{proxy_group}
 '''.format(
             proxy_group=self.proxy_group
         )
