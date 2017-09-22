e5355c2e71f5ad34972caa8aece25b1fb5aff7bd
Terry Chan <terry@32ph.com>
2017-09-23 02:30:18 +0800

    add rules for WeChat

---
+++ src/rules/wechat.py
@@ -0,0 +1,16 @@
+from rules.rules import Rules
+
+
+class WeChat(Rules):
+    def __str__(self):
+        return '''# Rule for WeChat
+USER-AGENT,*MicroMessenger*,{proxy_group}
+USER-AGENT,*WeChat*,{proxy_group}
+DOMAIN-SUFFIX,mmbiz.qpic.cn,{proxy_group}
+DOMAIN-SUFFIX,weixin.qq.com,{proxy_group}
+DOMAIN-SUFFIX,wx.qq.com,{proxy_group}
+DOMAIN-SUFFIX,weixinbridge.com,{proxy_group}
+DOMAIN-SUFFIX,servicewechat.com,{proxy_group}
+'''.format(
+            proxy_group=self.proxy_group
+        )
