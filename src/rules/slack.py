aee27fef32f554f1802fe2370d6944c125ce9fbf
Terry Chan <terry@32ph.com>
2017-12-18 14:37:38 +0800

    update slack rules

--- src/rules/slack.py
+++ src/rules/slack.py
@@ -6,6 +6,8 @@ class Slack(Rules):
         return '''# Rule for Slack
 DOMAIN-SUFFIX,slack.com,{proxy_group}
 DOMAIN-SUFFIX,slack-edge.com,{proxy_group}
+USER-AGENT,com.tinyspeck.chatlyio/*,{proxy_group}
+USER-AGENT,Slack/*,{proxy_group}
 '''.format(
             proxy_group=self.proxy_group
         )
